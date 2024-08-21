76ab04ead2659a8a035c3eddae8aa9b8d8b7b3fb
Konstantin Dimitrov <konstantin@algorithmity.com>
2024-08-21 11:57:48 +0300

    Added tests for animal shelter

--- src/animalShelter/AnimalShelter.ts
+++ src/animalShelter/AnimalShelter.ts
@@ -7,6 +7,14 @@ import Tortoise from "../animals/Tortoise";
 import Snake from "../animals/Snake";
 import Animal from "../hierarchy/Animal";
 import Reptile from "../hierarchy/Reptile";
+import {
+  FAILED_TO_REMOVE_ANIMAL_FROM_FAMILY,
+  NEGATIVE_LIMIT_ERROR_MESSAGE,
+  NEW_MAMMAL_LIMIT_LESS_THAN_CURRENT_MAMMALS,
+  NEW_REPTILE_LIMIT_LESS_THAN_CURRENT_REPTILES,
+  NO_SPACE_FOR_MORE_MAMMALS,
+  NO_SPACE_FOR_MORE_REPTILES,
+} from "../../messages/errorMessages";
 const DEFAULT_MAMMAL_LIMIT = 20;
 const DEFAULT_REPTILE_LIMIT = 19;
 export default class AnimalShelter {
@@ -16,8 +24,7 @@ export default class AnimalShelter {
 
   private animals: Animal[] = [];
 
-  private constructor() { }
-
+  private constructor() {}
 
   static getInstance() {
     if (!AnimalShelter.instance) {
@@ -25,84 +32,90 @@ export default class AnimalShelter {
     }
     return AnimalShelter.instance;
   }
+  static resetInstance() {
+    AnimalShelter.instance = new AnimalShelter();
+  }
 
   private getMammalsCount() {
-    return this.animals.filter(x => x instanceof Mammal).length;
+    return this.animals.filter((x) => x instanceof Mammal).length;
   }
 
   getMammalLimit() {
     return this._mammalLimit;
   }
-  setMammalLimit(newMammalLimit: number) {
+  setMammalLimit(newMammalLimit: number): string {
     if (newMammalLimit < 0) {
-      console.log(`Limit cannot be negative`);
-      return;
+      return NEGATIVE_LIMIT_ERROR_MESSAGE;
     }
     const mamalsTotal = this.getMammalsCount();
     if (newMammalLimit >= mamalsTotal) {
       this._mammalLimit = newMammalLimit;
-      console.log("New Mammal Limit is now : " + this._mammalLimit);
+      return "Succesfully set new mammal limit";
     } else {
-      console.log(
-        `Cannot change mammal limit to ${newMammalLimit} there are already ${mamalsTotal} mammals`
-      );
+      return NEW_MAMMAL_LIMIT_LESS_THAN_CURRENT_MAMMALS;
     }
   }
-
   private getReptileCount() {
-    return this.animals.filter(x => x instanceof Reptile).length;
+    return this.animals.filter((x) => x instanceof Reptile).length;
   }
   getReptileLimit() {
     return this._reptileLimit;
   }
-  setReptileLimit(newReptileLimit: number) {
+  setReptileLimit(newReptileLimit: number): string {
     if (newReptileLimit < 0) {
-      console.log(`Limit cannot be negative`);
-      return;
+      return `Limit cannot be negative`;
     }
     const reptilesTotal = this.getReptileCount();
     if (newReptileLimit >= reptilesTotal) {
       this._reptileLimit = newReptileLimit;
-      console.log("New Reptile Limit is now : " + this._reptileLimit);
+      return "Succesfully set new reptile limit";
     } else {
-      console.log(
-        `Cannot change reptile limit to ${newReptileLimit} there ${reptilesTotal == 1 ? "is" : "are"} already ${reptilesTotal} ${reptilesTotal == 1 ? "reptile" : "reptile"}`
-      );
+      return NEW_REPTILE_LIMIT_LESS_THAN_CURRENT_REPTILES;
     }
   }
-
 
   showAnimals() {
-
-    console.log(this.animals);
-    console.log("mammals: " + this.getMammalsCount());
-    console.log("reptiles: " + this.getReptileCount());
-    console.log(`lions: ${this.animals.filter(lion => lion instanceof Lion).length}`);
-    console.log(`crocodiles: ${this.animals.filter(crocodile => crocodile instanceof Crocodile).length}`);
-    console.log(`squirrels: ${this.animals.filter(squirrel => squirrel instanceof Squirrel).length}`);
-    console.log(`snakes: ${this.animals.filter(snake => snake instanceof Snake).length}`);
-    console.log(`tortoises: ${this.animals.filter(tortoise => tortoise instanceof Tortoise).length}`);
+    const animalNumbers: {
+      mamamls: number;
+      reptiles: number;
+      lions: number;
+      crocodiles: number;
+      squirrels: number;
+      snakes: number;
+      tortoises: number;
+    } = {
+      mamamls: this.getMammalsCount(),
+      reptiles: this.getReptileCount(),
+      lions: this.animals.filter((lion) => lion instanceof Lion).length,
+      crocodiles: this.animals.filter(
+        (crocodile) => crocodile instanceof Crocodile
+      ).length,
+      squirrels: this.animals.filter((squirrel) => squirrel instanceof Squirrel)
+        .length,
+      snakes: this.animals.filter((snake) => snake instanceof Snake).length,
+      tortoises: this.animals.filter((tortoise) => tortoise instanceof Tortoise)
+        .length,
+    };
+    return animalNumbers;
   }
 
-  addAnimal(animal: Animal): void {
-
-    if (animal instanceof Reptile && this.getReptileCount() + 1 > this._reptileLimit) {
-      console.error(
-        "Sorry, there is no space for another reptile in the Animal Shelter!"
-      );
-      return;
+  addAnimal(animal: Animal): string {
+    if (
+      animal instanceof Reptile &&
+      this.getReptileCount() + 1 > this._reptileLimit
+    ) {
+      return NO_SPACE_FOR_MORE_REPTILES;
+    } else if (
+      animal instanceof Mammal &&
+      this.getMammalsCount() + 1 > this._mammalLimit
+    ) {
+      return NO_SPACE_FOR_MORE_MAMMALS;
     }
-    else if (animal instanceof Mammal && this.getMammalsCount() + 1 > this._mammalLimit) {
-      console.error(
-        "Sorry, there is no space for another mammal in the Animal Shelter!"
-      );
-      return;
-    }
-    const family = families.find(pack => pack.name === animal.getHome());
+    const family = families.find((pack) => pack.name === animal.getHome());
     if (!family || family.removeAnimal(animal)) {
       this.animals.push(animal);
       animal.setHome("Animal Shelter");
-      console.log(`${animal.getName()} has been added to the Animal Shelter`);
-    }
+      return `${animal.getName()} has been added to the Animal Shelter`;
+    } else return FAILED_TO_REMOVE_ANIMAL_FROM_FAMILY;
   }
 }
